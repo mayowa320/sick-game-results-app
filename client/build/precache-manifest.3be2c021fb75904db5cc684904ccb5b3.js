0167f45e774b8b9cb5e4d3e3bde17fa56cfc757e
Mayowa <mayowa320@gmail.com>
2022-10-28 13:08:31 -0400

    updated build folder

---
+++ client/build/precache-manifest.3be2c021fb75904db5cc684904ccb5b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac8850e4c0af9bea8f7e4834286d879f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e86c71d1e8d97e3e9122",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "9bf22e8ba8e51295d98f",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "e86c71d1e8d97e3e9122",
+    "url": "/static/js/2.520c74f1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.520c74f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf22e8ba8e51295d98f",
+    "url": "/static/js/main.4b0d81a1.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
