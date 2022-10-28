b5f902090357fc99ab0cdc02b27204778546113e
Mayowa <mayowa320@gmail.com>
2022-10-28 13:34:03 -0400

    fixed static

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ server.start().then(() => {
 
 // // if we're in production, serve client/build as static assets
 //if (process.env.NODE_ENV === "production") {
-// app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 //}
 
 // app.use(routes);
